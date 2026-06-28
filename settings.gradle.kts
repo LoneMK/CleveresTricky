874e53b75bf56bf409e15f7dc5d48e03bc4adbf9
tryigit <info@tryigit.dev>
2026-06-28 06:12:58 +0300

    security: mitigate dependabot alerts by forcing safe versions, add cargo audit step and ecosystem

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,14 +24,26 @@ gradle.rootProject {
     allprojects {
         buildscript.configurations.all {
             resolutionStrategy {
-                force("io.netty:netty-codec-http:4.2.15.Final")
-                force("io.netty:netty-codec-http2:4.2.15.Final")
+                force("io.netty:netty-codec-http:4.1.118.Final")
+                force("io.netty:netty-codec-http2:4.1.118.Final")
+                force("io.netty:netty-codec:4.1.118.Final")
+                force("io.netty:netty-handler-proxy:4.1.118.Final")
+                force("org.bouncycastle:bcpkix-jdk18on:1.80")
+                force("org.bouncycastle:bcprov-jdk18on:1.80")
+                force("ch.qos.logback:logback-core:1.5.16")
+                force("ch.qos.logback:logback-classic:1.5.16")
             }
         }
         configurations.all {
             resolutionStrategy {
-                force("io.netty:netty-codec-http:4.2.15.Final")
-                force("io.netty:netty-codec-http2:4.2.15.Final")
+                force("io.netty:netty-codec-http:4.1.118.Final")
+                force("io.netty:netty-codec-http2:4.1.118.Final")
+                force("io.netty:netty-codec:4.1.118.Final")
+                force("io.netty:netty-handler-proxy:4.1.118.Final")
+                force("org.bouncycastle:bcpkix-jdk18on:1.80")
+                force("org.bouncycastle:bcprov-jdk18on:1.80")
+                force("ch.qos.logback:logback-core:1.5.16")
+                force("ch.qos.logback:logback-classic:1.5.16")
             }
         }
     }
